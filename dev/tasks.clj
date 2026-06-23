840d553593dc15df571e47bfe191fc2a2e81c2ef
Michael Rose <michael@rosenetwork.net>
2026-06-23 01:33:08 -0700

    took over css task so I could easily make it find local or remote gesso

--- dev/tasks.clj
+++ dev/tasks.clj
@@ -1,14 +1,66 @@
 (ns tasks
-  (:require [com.biffweb.tasks :as tasks]))
+  (:require
+   [clojure.string :as str]
+   [com.biffweb.tasks :as biff-tasks]))
 
 (defn hello
-  "Says 'Hello'"
+  "Says 'Hello'."
   []
   (println "Hello"))
 
-;; Tasks should be vars (#'hello instead of hello) so that `clj -M:dev help` can
-;; print their docstrings.
-(def custom-tasks
-  {"hello" #'hello})
+(defn- gesso-source-root
+  []
+  (let [path-for
+        (requiring-resolve
+         'gesso.build.find-path/path-for)
 
-(def tasks (merge tasks/tasks custom-tasks))
+        source-root
+        (some-> (path-for "gesso")
+                str
+                (str/replace "\\" "/"))]
+    (or source-root
+        (throw
+         (ex-info
+          "Could not resolve the Gesso source root from the active classpath."
+          {})))))
+
+(defn css
+  "Build CSS with Biff's normal Tailwind task while adding the Gesso source
+   tree selected by the active Clojure classpath.
+
+   Examples:
+
+     clj -M:dev css
+       Uses the pinned remote Gesso dependency.
+
+     clj -M:local-gesso:dev css
+       Uses ../gesso.
+
+   Additional Tailwind arguments such as --watch and --minify are forwarded
+   unchanged."
+  [& args]
+  (let [content
+        (str/join
+         ","
+         ["./src/**/*"
+          "./resources/**/*"
+          (str (gesso-source-root) "/**/*")])]
+    (apply
+     biff-tasks/css
+     (concat
+      ["--content" content]
+      args))))
+
+;; Tasks should be vars (#'hello instead of hello) so that `clj -M:dev help`
+;; can print their docstrings.
+;;
+;; Merging custom-tasks last intentionally replaces Biff's built-in "css"
+;; task. Biff's existing dev, uberjar, deploy, and soft-deploy tasks all invoke
+;; the task named "css", so they automatically use this override.
+(def custom-tasks
+  {"hello" #'hello
+   "css" #'css})
+
+(def tasks
+  (merge biff-tasks/tasks
+         custom-tasks))
